5b2c7446e06a881b82e37c7af42dbdd9a3dad3da
rodrigo <rob.developer@protonmail.com>
2021-06-03 19:53:16 -0400

    add: request method

--- mars_rover_photos.rb
+++ mars_rover_photos.rb
@@ -1,15 +1,20 @@
 require "uri"
 require "net/http"
+require "json"
 
 
-def request()
+def request(address, api_key)
 
-url = URI("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=Qa0f87BLeVOEqEozV8DTuCaUca0IAefW3kbCXq5Y")
+    url = URI("#{address}&api_key=#{api_key}")
 
-https = Net::HTTP.new(url.host, url.port)
-https.use_ssl = true
+    https = Net::HTTP.new(url.host, url.port)
+    https.use_ssl = true
 
-request = Net::HTTP::Get.new(url)
+    request = Net::HTTP::Get.new(url)
 
-response = https.request(request)
-puts response.read_body
+    response = https.request(request)
+    body = JSON.parse response.read_body
+
+end
+
+pp request("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000", "Qa0f87BLeVOEqEozV8DTuCaUca0IAefW3kbCXq5Y")
